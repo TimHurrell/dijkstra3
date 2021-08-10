3d91d99ec47de1199b2ff2ccb3f04858453914d6
Tim Hurrell <Tim@Hurrell.org>
2021-08-10 20:52:27 +0100

    added class library and console application. The later doesn't work yet

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numInput1 = "";
+            Console.Write("Enter start word : ");
+            Console.Write("Enter finish word : ");
+            Console.Write("Enter word list : ");
+            List<String> wordList = new List<string> { "hot", "dot", "dog", "lot", "log", "cog" };
+            string beginWord = "hit";
+            string endWord = "cog";
+            //numInput1 = Console.ReadLine();
+            Solution sol1 = new Solution();
+            Console.WriteLine("The outputs are {0}", sol1.FindLadders(beginWord, endWord, wordList));
+        }
+    }
+}
