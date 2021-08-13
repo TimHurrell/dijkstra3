13d888a227150d9bacb78c5e4a29e3ebbc07d8b9
Tim Hurrell <Tim@Hurrell.org>
2021-08-13 20:53:39 +0100

    console now works. Now need to apply spec

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,15 +10,23 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             string numInput1 = "";
-            Console.Write("Enter start word : ");
-            Console.Write("Enter finish word : ");
-            Console.Write("Enter word list : ");
+            Console.Write("Enter start word : \n");
+            Console.Write("Enter finish word :\n ");
+            Console.Write("Enter word list :\n ");
             List<String> wordList = new List<string> { "hot", "dot", "dog", "lot", "log", "cog" };
             string beginWord = "hit";
             string endWord = "cog";
             //numInput1 = Console.ReadLine();
             Solution sol1 = new Solution();
-            Console.WriteLine("The outputs are {0}", sol1.FindLadders(beginWord, endWord, wordList));
+
+            foreach (var item in sol1.FindLadders(beginWord, endWord, wordList))
+            {
+                Console.Write("New ladder \n");
+                foreach (var item1 in item)
+                {
+                    Console.WriteLine(item1.ToString());
+                }
+            }
         }
     }
 }
