c7127b88c717582cc11b37138c3f19882c0ebc68
Tim Hurrell <Tim@Hurrell.org>
2021-10-17 21:56:19 +0100

    Refactoring Console again to separate concerns. Create File now a separate class.

--- ConsoleApplication/Console.cs
+++ ConsoleApplication/Console.cs
@@ -2,56 +2,16 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using WordLadderLibrary;
 using InputwordClass;
 using WordlistClass;
-using WordladderstringClass;
+using WriteLaddersToFile;
 
 namespace ConsoleApplication
 {
     class Console
     {
-        public void Process()
-        {
-            InputWordsForWordLadders InputWordsForWordLaddersinstance = Intro();
-
-            System.Console.Write("Enter file name :\n ");
-            string filename = System.Console.ReadLine();
-            string filePath = Path.Combine(AppContext.BaseDirectory, filename);
-            Listofwordsfromwordfile wordlistinstance = CreateWordListFromInputWordFile(filePath);
-            FinishWordExistsInList(wordlistinstance, InputWordsForWordLaddersinstance.Finishword);
-
-
-            wordlistinstance.RemoveIncorrectLength(InputWordsForWordLaddersinstance.Seedword);
-            WordLadderSolution WordLadderInstance = new WordLadderSolution();
-            IList<IList<string>> ladders = WordLadderInstance.FindLadders(InputWordsForWordLaddersinstance.Seedword, InputWordsForWordLaddersinstance.Finishword, wordlistinstance._listofwordsfromwordfile);
-            WriteLaddersToCsvFile(ladders);
-        }
-        public InputWordsForWordLadders Intro()
-        {
-            System.Console.Write("Enter start word : \n");
-            InputWordsForWordLadders InputWordsForWordLaddersinstance = new InputWordsForWordLadders
-            {
-                Seedword = System.Console.ReadLine()
-            };
-            System.Console.Write("Enter finish word :\n ");
-            InputWordsForWordLaddersinstance.Finishword = System.Console.ReadLine();
-            System.Console.WriteLine(InputWordsForWordLaddersinstance.Seedword);
-            System.Console.WriteLine(InputWordsForWordLaddersinstance.Finishword);
-            if (InputWordsForWordLaddersinstance.AreWordsDifferentLength(InputWordsForWordLaddersinstance.Seedword, InputWordsForWordLaddersinstance.Finishword))
-            {
-                System.Console.WriteLine("Sorry, words need to be the same length");
-                return InputWordsForWordLaddersinstance;
-            }
-            if (InputWordsForWordLaddersinstance.AreWordsDifferent(InputWordsForWordLaddersinstance.Seedword, InputWordsForWordLaddersinstance.Finishword) == false)
-            {
-                System.Console.WriteLine("Sorry, words need to be different");
-                return InputWordsForWordLaddersinstance;
-            }
-            return InputWordsForWordLaddersinstance;
-        }
-
+       
         public void FinishWordExistsInList(Listofwordsfromwordfile wordlist, string finishword)
         {
             if (!wordlist.WordExistsInListFromWordFile(finishword))
@@ -67,27 +27,48 @@ namespace ConsoleApplication
             return new Listofwordsfromwordfile(_listofwordsfromwordfile);
         }
 
-        //TODO we can write a test for this, but we need to split the method up so that it returns a string
-        //we're not interested in testing that we can write a file but more interested in what the contents of the file are.
-
-        // Done
-        public void WriteLaddersToCsvFile(IList<IList<string>> ladders)
-        {
-            string strFilePath = Path.Combine(AppContext.BaseDirectory, "Data.csv");
-
-            StringOfWordLadder WordLadderString = new StringOfWordLadder();
-
-            File.WriteAllText(strFilePath, WordLadderString.GetStringOfWordLadder(ladders));
-        }
         static void Main()
         {
             Console console = new Console();
-            console.Process();
+            //InputWordsForWordLadders InputWordsForWordLaddersinstance = console.Intro();
+
+            System.Console.Write("Enter start word : \n");
+            InputWordsForWordLadders InputWordsForWordLaddersinstance = new InputWordsForWordLadders
+            {
+                Seedword = System.Console.ReadLine()
+            };
+            System.Console.Write("Enter finish word :\n ");
+            InputWordsForWordLaddersinstance.Finishword = System.Console.ReadLine();
+            System.Console.WriteLine(InputWordsForWordLaddersinstance.Seedword);
+            System.Console.WriteLine(InputWordsForWordLaddersinstance.Finishword);
+            if (InputWordsForWordLaddersinstance.AreWordsDifferentLength(InputWordsForWordLaddersinstance.Seedword, InputWordsForWordLaddersinstance.Finishword))
+            {
+                System.Console.WriteLine("Sorry, words need to be the same length");
+                return;
+            }
+            if (InputWordsForWordLaddersinstance.AreWordsDifferent(InputWordsForWordLaddersinstance.Seedword, InputWordsForWordLaddersinstance.Finishword) == false)
+            {
+                System.Console.WriteLine("Sorry, words need to be different");
+                return;
+            }
+
+
+            System.Console.Write("Enter file name :\n ");
+            string filename = System.Console.ReadLine();
+            string filePath = Path.Combine(AppContext.BaseDirectory, filename);
+            Listofwordsfromwordfile wordlistinstance = console.CreateWordListFromInputWordFile(filePath);
+            console.FinishWordExistsInList(wordlistinstance, InputWordsForWordLaddersinstance.Finishword);
+
+
+            wordlistinstance.RemoveIncorrectLength(InputWordsForWordLaddersinstance.Seedword);
+            WordLadderSolution WordLadderInstance = new WordLadderSolution();
+            IList<IList<string>> ladders = WordLadderInstance.FindLadders(InputWordsForWordLaddersinstance.Seedword, InputWordsForWordLaddersinstance.Finishword, wordlistinstance._listofwordsfromwordfile);
+            WriteLadders writeLadders = new WriteLadders();
+
+
+            writeLadders.WriteToFile(ladders);
         }
     }
-
-
-   
 
 
 }
---
+++ ConsoleApplication/WriteLaddersToFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using WordladderstringClass;
+
+namespace WriteLaddersToFile
+{
+    public class WriteLadders
+    {
+        public void WriteToFile(IList<IList<string>> ladders)
+        {
+            string strFilePath = Path.Combine(AppContext.BaseDirectory, "Data.csv");
+
+            StringOfWordLadder WordLadderString = new StringOfWordLadder();
+
+            File.WriteAllText(strFilePath, WordLadderString.GetStringOfWordLadder(ladders));
+        }
+    }
+}
+
